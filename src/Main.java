4ad546cda1704c7a21040b1b2faa0783eb9993fe
Dmitrii Volt <dmitriy.volt@gmail.com>
2024-03-02 23:58:40 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Task 1
+        System.out.println("Task 1");
+        int goal = 2_459_000;
+        int amount = 0;
+        int payment = 15000;
+        int month = 1;
+        while (amount <= goal) {
+            amount = amount + payment;
+            amount = amount + (amount/100);
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + amount + " рублей");
+            month++;
+        }
+        System.out.println();
     }
 }
